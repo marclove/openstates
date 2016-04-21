e171bf738ec74d7acf94c0605f657ce24411f575
Ivo Benedito <ivobenedito@gmail.com>
2016-04-21 14:50:18 +0100

    Implemented OpenStates::Client #legislators_geo endpoint

--- lib/openstates/client/legislators.rb
+++ lib/openstates/client/legislators.rb
@@ -2,11 +2,16 @@ module OpenStates
   class Client
     module Legislators
       def legislators(options = {})
-        get('legislators', options)
+        get("legislators", options)
       end
 
       def legislator(id, options = {})
         get("legislators/#{id}", options)
+      end
+
+      def legislators_geo(lat, long, options = {})
+        options.merge!(lat: lat, long: long)
+        get("legislators/geo", options)
       end
     end
   end

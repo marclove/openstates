53313c97c5602ef01010590f70b46c823c196664
Ivo Benedito <ivobenedito@gmail.com>
2016-04-21 14:55:46 +0100

    Implemented OpenStates::Client::Committees #committee detail endpoint

--- lib/openstates/client/committees.rb
+++ lib/openstates/client/committees.rb
@@ -4,6 +4,10 @@ module OpenStates
       def committees(options = {})
         get("committees", options)
       end
+
+      def committee(id, options = {})
+        get("committees/#{id}", options)
+      end
     end
   end
 end

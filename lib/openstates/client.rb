8e244caace79dae92708282d5a7c7eaa9fd1ef03
Ivo Benedito <ivobenedito@gmail.com>
2016-04-21 14:09:36 +0100

    Added #legislators call to OpenStastes::Client

--- lib/openstates/client.rb
+++ lib/openstates/client.rb
@@ -1,15 +1,19 @@
-require "congress/connection"
-require "congress/request"
+require "openstates/connection"
+require "openstates/request"
 
 module OpenStates
   class Client
-    include Congress::Connection
-    include Congress::Request
+    include OpenStates::Connection
+    include OpenStates::Request
 
-    attr_reader :key
+    attr_reader :api_key
 
-    def initialize(key)
-      @key = key
+    def initialize(api_key)
+      @api_key = api_key
+    end
+
+    def legislators(options = {})
+      get('legislators', options)
     end
   end
 end

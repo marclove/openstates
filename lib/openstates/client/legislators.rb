b9928a7c463216b2d4223bef4b21be11e3b8da68
Ivo Benedito <ivobenedito@gmail.com>
2016-04-21 14:42:53 +0100

    Implemented OpenStates::Client #legislator endpoint

--- lib/openstates/client/legislators.rb
+++ lib/openstates/client/legislators.rb
@@ -4,6 +4,10 @@ module OpenStates
       def legislators(options = {})
         get('legislators', options)
       end
+
+      def legislator(id, options = {})
+        get("legislators/#{id}", options)
+      end
     end
   end
 end

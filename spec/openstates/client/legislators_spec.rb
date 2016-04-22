1862f665bbe6acdddfc5fa4ab84f93ce1e0e99a5
Ivo Benedito <ivobenedito@gmail.com>
2016-04-22 14:16:04 +0100

    Minor fix

--- spec/openstates/client/legislators_spec.rb
+++ spec/openstates/client/legislators_spec.rb
@@ -11,7 +11,7 @@ describe OpenStates::Client::Legislators do
   end
 
   describe "#legislator", :vcr do
-    it "returns a legislator by id" do
+    it "returns legislator details by id" do
       legislator = client.legislator("AKL000031")
 
       expect(legislator.full_name).to eq("Les Gara")
@@ -22,7 +22,7 @@ describe OpenStates::Client::Legislators do
   describe "#legislators_geo", :vcr do
     it "returns legislators for a latitude and longitude pair" do
       legislators = client.legislators_geo(37.775, -122.418)
-      
+
       expect(legislators.first.full_name).not_to be_nil
       assert_requested :get, openstates_url("legislators/geo?lat=37.775&long=-122.418")
     end

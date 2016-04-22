f7c1d2fd13724f12f30782d5e726dd6e98ce6f13
Ivo Benedito <ivobenedito@gmail.com>
2016-04-22 15:30:15 +0100

    Implemented States spec

--- spec/openstates/client/states_spec.rb
+++ spec/openstates/client/states_spec.rb
@@ -1,11 +1,21 @@
 require "helper"
 
 describe OpenStates::Client::States do
-  describe "#states" do
-    pending
+  describe "#states", :vcr do
+    it "returns all states" do
+      states = client.states
+
+      expect(states.count).to be > 0
+      assert_requested :get, openstates_url("metadata")
+    end
   end
 
-  describe "#state" do
-    pending
+  describe "#state", :vcr do
+    it "returns state metadata by abbreviation" do
+      state = client.state("ny")
+
+      expect(state.name).to eq("New York")
+      assert_requested :get, openstates_url("metadata/ny")
+    end
   end
 end

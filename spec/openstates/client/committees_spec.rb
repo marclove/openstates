63a88bc56c9f9c5bc01bfa39da3f815dc765a373
Ivo Benedito <ivobenedito@gmail.com>
2016-04-22 14:29:10 +0100

    Implemented Committees spec with VCR support

--- spec/openstates/client/committees_spec.rb
+++ spec/openstates/client/committees_spec.rb
@@ -1,11 +1,21 @@
 require "helper"
 
 describe OpenStates::Client::Committees do
-  describe "#committees" do
-    pending
+  describe "#committees", :vcr do
+    it "returns all committess" do
+      committees = client.committees
+
+      expect(committees.count).to be > 0
+      assert_requested :get, openstates_url("committees")
+    end
   end
 
-  describe "#committee" do
-    pending
+  describe "#committee", :vcr do
+    it "returns committee details by id" do
+      committee = client.committee("AKC000001")
+
+      expect(committee.committee).to eq("Community & Regional Affairs")
+      assert_requested :get, openstates_url("committees/AKC000001")
+    end
   end
 end

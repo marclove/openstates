2b6fd431c17f2173d4b8b7708738a361d7ce8186
Ivo Benedito <ivobenedito@gmail.com>
2016-04-22 15:24:59 +0100

    Implemented Event specs

--- spec/openstates/client/events_spec.rb
+++ spec/openstates/client/events_spec.rb
@@ -1,11 +1,23 @@
 require "helper"
 
 describe OpenStates::Client::Events do
-  describe "#events" do
-    pending
+  describe "#events", :vcr do
+    it "returns all events" do
+      events = client.events
+
+      expect(events.count).to be > 0
+      assert_requested :get, openstates_url("events")
+    end
   end
 
-  describe "#event" do
-    pending
+  describe "#event", :vcr do
+    let(:description) { "Task Force Concerning Outcomes Based Financing Meeting (1E)" }
+
+    it "returns event details by id" do
+      event = client.event("CTE00129791")
+
+      expect(event.description).to eq(description)
+      assert_requested :get, openstates_url("events/CTE00129791")
+    end
   end
 end
